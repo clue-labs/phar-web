796af030bfeb791b9f5fa08b08b9616c162f0d03
Christian Lück <christian@lueck.tv>
2013-08-01 01:01:03 +0200

    Use symfony/process to stream output to redis

--- src/Clue/PharWeb/Job/Build.php
+++ src/Clue/PharWeb/Job/Build.php
@@ -5,6 +5,8 @@ namespace Clue\PharWeb\Job;
 use Clue\PharWeb\PackageManager;
 use Clue\PharWeb\Model\Build as BuildModel;
 use Resque;
+use Symfony\Component\Process\Process;
+use UnexpectedValueException;
 
 class Build
 {
@@ -19,18 +21,31 @@ class Build
         $build = $manager->getBuild($bid);
 
         if ($build->getStatus() !== BuildModel::STATUS_PENDING) {
-            throw new \UnexpectedValueException('Build in invalid state "' . $build->getStatus() . '", expected "' . BuildModel::STATUS_PENDING . '"');
+            throw new UnexpectedValueException('Build in invalid state "' . $build->getStatus() . '", expected "' . BuildModel::STATUS_PENDING . '"');
         }
         $build->setStatus(BuildModel::STATUS_PROCESSING);
 
-        chdir('/home/me/workspace/phar-composer/');
-        exec('php -d phar.readonly=off bin/phar-composer build ' . escapeshellarg($package . ':' . $version) . ' ' . escapeshellarg($outfile) . ' 2>&1', $lines, $code);
+        $ok = true;
 
-        $build->setLog(implode(PHP_EOL, $lines));
+        $process = new Process('php -d phar.readonly=off bin/phar-composer build ' . escapeshellarg($package . ':' . $version) . ' ' . escapeshellarg($outfile), '/home/me/workspace/phar-composer/');
+        $process->start();
+        $code = $process->wait(function($type, $data) use ($build, &$ok) {
+            $build->addLog($data);
+
+            if ($type !== Process::OUT) {
+                $ok = false;
+            }
+        });
+
+        if (!$ok) {
+            throw new UnexpectedValueException('Error running composer');
+        }
+
+        //$build->setLog(implode(PHP_EOL, $lines));
 
         if ($code !== 0) {
             $build->setStatus(BuildModel::STATUS_ERROR);
-            throw new \UnexpectedValueException('Build error');
+            throw new UnexpectedValueException('Build error');
         }
 
         $build->setStatus(BuildModel::STATUS_OK);
